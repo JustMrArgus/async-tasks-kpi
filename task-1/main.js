80f3f9f88477a77259ee65a2e98eb23ba196b07b
Argus <argusprogramming@protonmail.com>
2024-11-07 10:36:52 +0000

    Add task 1 solution

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,42 @@
-const callbackFilter = (userArray, callback) => {
-  const filteredAray = [];
-  for (element of userArray) {
-    if (callback(element) === true) {
-      filteredAray.push(element);
+const CallbackBasedFilter = (userArray, callback) => {
+  const filteredArray = [];
+  let arrayIndex = 0;
+
+  const nextElementCheck = () => {
+    if (arrayIndex < userArray.length) {
+      const element = userArray[arrayIndex++];
+
+      setTimeout(() => {
+        if (callback(element)) {
+          filteredArray.push(element);
+        }
+        nextElementCheck();
+      }, 200);
+
+    } 
+    else {
+      console.log(filteredArray);
     }
-  }
-  return filteredAray;
-}
+  };
+
+  nextElementCheck();
+};
+
+// Test cases
+let firstArray = [1, 2, 3, 4, 5, 6];
+let secondArray = ['a', 'b', 'c', 'd', 'h', 'w'];
+
+CallbackBasedFilter(
+  firstArray,
+  (x) => x % 2 === 0
+);
+
+CallbackBasedFilter(
+  firstArray,
+  (y) => y > 2
+)
+
+CallbackBasedFilter(
+  secondArray,
+  (n) => 'hello world'.split('').includes(n)
+)
